d7b957e6fd19f637b6c5388a89dedf49906821da
Samuel vermeuln <vermeuln@ucl.br>
2021-03-05 17:03:08 -0300

    evolução

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,13 @@
+import Head from 'next/head';
+
+import styles from '../styles/Components/Login.module.css'
 
 export default function login () {
     return (
-    <div>
+    <div className={styles.container}>
+        <Head>
+            <title>Tela de Login</title>
+        </Head>
         
     </div>
     );
