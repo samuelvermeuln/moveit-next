3e3502e3501956fa35e8fd1e1b8cc6d196c9f66f
samuel vermeuln <70477826+samuelvermeuln@users.noreply.github.com>
2021-12-28 21:36:28 -0300

    ............................

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const { resolve } = require('path')
 
 const app = express()
 
-app.use("/", express.static(resolve(
-    __dirname,
-    './build'
-)));
+// app.use("/", express.static(resolve(
+//     __dirname,
+//     './build'
+// )));
 
 app.listen(process.env.PORT || 3000, (err) => {
     if (err) { return console.log("ERROR FOI =>> ", err) }
